48ce1a7dc1fd283f250338e96a6e2e386876485e
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2025-01-23 14:03:44 +0330

    Update [required_license].tsx

--- src/pages/deploy/[app]/[required_license].tsx
+++ src/pages/deploy/[app]/[required_license].tsx
@@ -2,9 +2,9 @@
 // This file is owned by you, feel free to edit as you see fit.
 import * as React from "react";
 import { PageParamsProvider as PageParamsProvider__ } from "@plasmicapp/react-web/lib/host";
-import GlobalContextsProvider from "../../../components/plasmic/app_store/PlasmicGlobalContextsProvider";
+import GlobalContextsProvider from "../../../../components/plasmic/app_store/PlasmicGlobalContextsProvider";
 
-import { PlasmicDeploy } from "../../../components/plasmic/app_store/PlasmicDeploy";
+import { PlasmicDeploy } from "../../../../components/plasmic/app_store/PlasmicDeploy";
 import { useRouter } from "next/router";
 
 function Deploy() {
